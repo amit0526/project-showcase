5093e017462367078c9b8cb7edafda34bbb635f7
Amit Anand <amit.anand0526@gmail.com>
2026-08-05 14:45:29 +0530

    Add footer in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import FeaturedProjects from "../components/FeaturedProjects";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <FeaturedProjects />
+      <Footer />
     </>
   );
 }
